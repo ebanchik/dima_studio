d2a3f4a22fa6292cb0d7dd2fbdd0d0f687ae0cf7
ebanchik <ebanchik@gmail.com>
2024-03-15 17:52:29 -0400

    add about

---
+++ src/About.jsx
@@ -0,0 +1,20 @@
+export function About() {
+
+  return (
+    <section className="about" id="about">
+        <h1>About Me</h1>
+        <div className="about-container">
+          <div className="about-body">
+            <h3>Hello, I'm Eli, a New York City native and a recent graduate from NYU Gallatin with a diverse background in Business, Philosophy, and Psychology. My journey into the tech world began organically, sparked by my experiences in a marketing internship at a fintech startup where creativity thrived. I directed creative projects, conducted research, wrote compelling copy, and regularly pitched ideas. My analytical side shines through roles in investment analysis at EMBEE and data management at NewLab.
+            <br />
+            <br />
+            Personally, I've co-founded an event production company in high school, contributed to a large tech incubator, emerged victorious in a startup sprint, and completed a coding bootcamp. The allure of coding hit me during that sprint, witnessing my teammates transform an idea into an MVP in a day. It was a revelation – a perfect blend of creativity and skill. Since then, I've wholeheartedly committed myself to coding, seeking a workplace where I can continue to learn and grow.
+            <br />
+            <br />
+
+            At 23, I'm eager to embark on a software engineering journey, embracing challenges and contributing to innovative solutions. With a solid foundation in both creativity and logic, I am ready to bring a unique perspective to any team. I value collaborative learning, and at this early stage in my career, I'm on the lookout for a company culture that fosters personal and professional development. Let's build something great together.</h3>
+          </div>
+      </div>
+    </section>
+  )
+}
--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { Header } from "./Header";
 // import { Content } from "./Content";
 import { Footer } from "./Footer";
+import { About } from "./About";
 
 function App() {
   return (
     <div>
       <Header />
+      <About />
       {/* <Footer /> */}
     </div>
   )
