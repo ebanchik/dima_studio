e01b9643c17e3a550e2b331c01f29bd7ed0dc3d3
ebanchik <ebanchik@gmail.com>
2024-03-16 18:27:04 -0400

    move carousel

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,13 @@ import { Header } from "./Header";
 // import { Content } from "./Content";
 // import { Footer } from "./Footer";
 import { About } from "./About";
-import { Carousel } from "./Carousel";
+// import { Carousel } from "./Carousel";
 
 function App() {
-  const images = [
-    "/assets/images/chair.png",
-    "/assets/images/dining_room.png",
-    "/assets/images/living_room.png",
-  ];
 
   return (
     <div>
       <Header />
-      <Carousel images={images} />
       <About />
     </div>
   );
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,8 +1,17 @@
 // Header.js
+import Carousel from './Carousel';
+
 export function Header() {
+  const images = [
+    "/assets/images/chair.png",
+    "/assets/images/dining_room.png",
+    "/assets/images/living_room.png",
+  ];
+  
   return (
     <header className="header-container">
     <img src="/assets/013_381.jpeg" alt="Background" className="header-background" />
+    <Carousel images={images} />
     <h1>Dima Studio</h1>
     <nav>
       <ul>
